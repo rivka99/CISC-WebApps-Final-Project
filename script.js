f322876f128a91a0dd7e69d76bb20c7304e2b58e
rivka99 <rivkafl99@gmail.com>
2022-08-11 03:42:34 -0400

    added color change to localstorage

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const submitEl = document.getElementById("submit-btn");
+const colorFormeEl = document.querySelector(".color-form");
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (localStorage.getItem("favcolor")) {
+    colorFormeEl.style.visibility = "hidden";
+    let savedFave = JSON.parse(localStorage.getItem("favcolor"));
+    if (savedFave === "black") {
+      document.querySelector("body").style.color = "white";
+    }
+    document.querySelector("body").style.backgroundColor = savedFave;
+  }
+});
 
 submitEl.addEventListener("click", function (e) {
   e.preventDefault();
@@ -6,18 +18,19 @@ submitEl.addEventListener("click", function (e) {
     "form input[type=radio]:checked"
   );
   console.log(selectedItems);
-  console.log(
-    getMaxValueKey(
-      mode([
-        selectedItems[1].classList[0],
-        selectedItems[2].classList[0],
-        selectedItems[3].classList[0],
-        selectedItems[4].classList[0],
-        selectedItems[5].classList[0],
-        selectedItems[6].classList[0],
-      ])
-    )
+  let userPersonalColor = getMaxValueKey(
+    mode([
+      selectedItems[1].classList[0],
+      selectedItems[2].classList[0],
+      selectedItems[3].classList[0],
+      selectedItems[4].classList[0],
+      selectedItems[5].classList[0],
+      selectedItems[6].classList[0],
+    ])
   );
+  localStorage.setItem("favcolor", JSON.stringify(userPersonalColor));
+  console.log(userPersonalColor);
+  document.querySelector("body").style.backgroundColor = userPersonalColor;
 
   return true;
 });
