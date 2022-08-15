884cb52c4fe7c9f5b5a3a771307d99a9ec892f99
rivka99 <rivkafl99@gmail.com>
2022-08-14 21:49:06 -0400

    adding color picker/css

--- colors.js
+++ colors.js
@@ -10,7 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
   let colorCells = document.querySelectorAll(".color-cell");
   colorCells.forEach((cell) => {
     cell.style.backgroundColor =
-      "#" + Math.floor(Math.random() * 16777215).toString(16);
+      "#" +
+      "000000".replace(/0/g, function () {
+        return (~~(Math.random() * 16)).toString(16);
+      });
     cell.addEventListener("click", function () {
       cell.style.backgroundColor = document.getElementById("colorpick").value;
     });
